e4fdfcb8c8ad4dc9c9d905d913ee9a53fef44590
Woody <devkritt@gmail.com>
2024-03-21 20:42:19 +0700

    DEPLOY_DEMO_01

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 import { connectToDb } from "./connectToDb.js";
 import authRoute from './src/routers/auth.route.js'
@@ -12,8 +13,10 @@ import transactionRoute from './src/routers/transaction.route.js'
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/client/dist")))
 app.use(cookieParser());
 
 app.use(`/api/auth`, authRoute)
@@ -21,6 +24,10 @@ app.use(`/api/workspace`, workspaceRoute)
 app.use(`/api/product`, productRoute)
 app.use(`/api/order`, orderRoute)
 app.use(`/api/transaction`, transactionRoute)
+
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 app.listen(process.env.PORT, () => {
   connectToDb();
